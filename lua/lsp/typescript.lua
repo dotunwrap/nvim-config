02f5ab4341465a36e367013dfdc89b82cc826a66
Gabby Simpson <gabby@dotunwrap.dev>
2025-10-15 21:14:09 -0400

    feat(lsp): Add various filetypes for ts_ls

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -2,7 +2,19 @@ return {
 	ensure_installed = { "ts_ls" },
 	servers = {
 		ts_ls = {
-			config = {},
+			config = {
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+					"svelte",
+					"vue",
+					"astro",
+				},
+			},
 		},
 	},
 }

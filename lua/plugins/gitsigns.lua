b7e90d12e0440f13d3e159f51bc1ed913587918c
Gabby Simpson <gabby@dotunwrap.dev>
2025-10-21 13:47:13 -0400

    feat(plugins): Add keymaps for gitsigns.nvim

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,21 @@
 return {
-	{ "lewis6991/gitsigns.nvim" },
+	{
+		"lewis6991/gitsigns.nvim",
+		keys = {
+			{
+				"<leader>gb",
+				function()
+					require("gitsigns").blame_line()
+				end,
+				desc = "Git blame line",
+			},
+			{
+				"<leader>gd",
+				function()
+					require("gitsigns").diffthis()
+				end,
+				desc = "Git diff",
+			},
+		},
+	},
 }
